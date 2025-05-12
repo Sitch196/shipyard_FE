dfb1db90fc48ba69bfe16d2dbeb0f27c155a8289
Sitch196 <sichinava1996@gmail.com>
2025-05-12 18:39:53 +0400

    [fix:Frontend]: vite error

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -11,13 +11,16 @@ const Login = () => {
 
   const handleLogin = async (values: any, { setSubmitting }: any) => {
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
+      );
 
       if (response.ok) {
         const data = await response.json();
